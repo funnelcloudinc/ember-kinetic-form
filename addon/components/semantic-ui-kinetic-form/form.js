1aadaffda78f5152cd792005a1825bccc79ddb93
Devin Weaver <suki@tritarget.org>
2017-09-25 21:21:53 -0400

    Add semantic-ui-kinetic-form/form component

---
+++ addon/components/semantic-ui-kinetic-form/form.js
@@ -0,0 +1,7 @@
+import KineticFormFormComponent from '../kinetic-form/form';
+import layout from '../../templates/components/semantic-ui-kinetic-form/form';
+
+export default KineticFormFormComponent.extend({
+  layout,
+  classNames: ['semantic-ui-kinetic-form--form']
+});
---
+++ tests/integration/components/semantic-ui-kinetic-form/form-test.js
@@ -0,0 +1,42 @@
+import Ember from 'ember';
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import page from '../../../pages/components/semantic-ui-kinetic-form/form';
+import sinon from 'sinon';
+
+const { run, set } = Ember;
+
+moduleForComponent('semantic-ui-kinetic-form/form', 'Integration | Component | semantic ui kinetic form/form', {
+  integration: true,
+  beforeEach() {
+    page.setContext(this);
+  },
+  afterEach() {
+    page.removeContext();
+  }
+});
+
+test('yields a block', function (assert) {
+  page.render(hbs`{{#semantic-ui-kinetic-form/form}}Test block content{{/semantic-ui-kinetic-form/form}}`);
+  assert.ok(page.text.includes('Test block content'), 'expected component to render block content');
+});
+
+test('displays a title', function(assert) {
+  page.render(hbs`{{semantic-ui-kinetic-form/form title="test-title"}}`);
+  assert.equal(page.title, 'test-title');
+});
+
+test('disables submit button when isInvalid is true', function (assert) {
+  page.render(hbs`{{semantic-ui-kinetic-form/form isInvalid=false}}`);
+  assert.ok(page.submitButton.isEnabled, 'expected submit button to be enabled');
+  page.render(hbs`{{semantic-ui-kinetic-form/form isInvalid=true}}`);
+  assert.ok(page.submitButton.isDisabled, 'expected submit button to be disabled');
+});
+
+test('calls onSubmit action when user clicks submit button', function () {
+  let onSubmitSpy = sinon.spy();
+  set(this, 'onSubmitSpy', onSubmitSpy);
+  page.render(hbs`{{semantic-ui-kinetic-form/form onSubmit=(action onSubmitSpy)}}`);
+  run(() => page.submitButton.click());
+  sinon.assert.calledOnce(onSubmitSpy);
+});
