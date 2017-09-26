2f201b8bf49d7172fb67b539c9537982906e03c7
Devin Weaver <suki@tritarget.org>
2017-09-25 21:52:40 -0400

    Add semantic-ui-kinetic-form/string component

---
+++ addon/components/semantic-ui-kinetic-form/string.js
@@ -0,0 +1,7 @@
+import KineticFormStringComponent from '../kinetic-form/string';
+import layout from '../../templates/components/semantic-ui-kinetic-form/string';
+
+export default KineticFormStringComponent.extend({
+  layout,
+  classNames: ['semantic-ui-kinetic-form--string', 'item']
+});
