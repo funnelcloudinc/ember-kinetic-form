3b9196d0cb016e079e5272b2b13e39d2bec2cc34
Devin Weaver <suki@tritarget.org>
2017-09-25 22:45:30 -0400

    Add kinetic-form/textarea component

---
+++ tests/pages/components/kinetic-form/textarea.js
@@ -0,0 +1,20 @@
+import PageObject from 'ember-cli-page-object';
+
+const { text, value, fillable, contains, hasClass } = PageObject;
+
+export const KineticFormTextarea = {
+  scope: '.kinetic-form--textarea',
+  title: text('label'),
+  value: value('textarea'),
+  enterText: fillable('textarea'),
+  hasInTitle: contains('label'),
+  hasError: hasClass('has-error'),
+  isRequired: {
+    isDescriptor: true,
+    get() {
+      return this.hasInTitle('*');
+    }
+  }
+};
+
+export default PageObject.create(KineticFormTextarea);
