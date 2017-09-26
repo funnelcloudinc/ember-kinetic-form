f2b6855c36892a5979108b815a4b3c8fb3b81d8b
Devin Weaver <suki@tritarget.org>
2017-09-25 22:39:19 -0400

    Add semantic-ui-kinetic-form/number component

---
+++ addon/components/semantic-ui-kinetic-form/number.js
@@ -0,0 +1,7 @@
+import KineticFormNumberComponent from '../kinetic-form/number';
+import layout from '../../templates/components/semantic-ui-kinetic-form/number';
+
+export default KineticFormNumberComponent.extend({
+  layout,
+  classNames: ['semantic-ui-kinetic-form--number', 'item']
+});
