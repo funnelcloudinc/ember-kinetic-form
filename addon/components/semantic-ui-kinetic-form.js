b16ae5a82a23c26d97e6a4ba71247639f5e4952a
Devin Weaver <suki@tritarget.org>
2017-09-30 19:05:21 -0400

    Add main semantic-ui-kinetic-form component

---
+++ addon/components/semantic-ui-kinetic-form.js
@@ -0,0 +1,12 @@
+import KineticFormComponent from './kinetic-form';
+
+export default KineticFormComponent.extend({
+  classNames: ['semantic-ui-kinetic-form'],
+  errorComponent: 'semantic-ui-kinetic-form/errors',
+  formComponent: 'semantic-ui-kinetic-form/form',
+  stringComponent: 'semantic-ui-kinetic-form/string',
+  numberComponent: 'semantic-ui-kinetic-form/number',
+  booleanComponent: 'semantic-ui-kinetic-form/boolean',
+  radiosComponent: 'semantic-ui-kinetic-form/radios',
+  textareaComponent: 'semantic-ui-kinetic-form/textarea'
+});
---
+++ tests/pages/components/semantic-ui-kinetic-form.js
@@ -0,0 +1,28 @@
+import PageObject from 'ember-cli-page-object';
+import { SemanticUIKineticFormErrors } from './semantic-ui-kinetic-form/errors';
+import { SemanticUIKineticFormForm } from './semantic-ui-kinetic-form/form';
+import { SemanticUIKineticFormBoolean } from './semantic-ui-kinetic-form/boolean';
+import { SemanticUIKineticFormNumber } from './semantic-ui-kinetic-form/number';
+import { SemanticUIKineticFormRadios } from './semantic-ui-kinetic-form/radios';
+import { SemanticUIKineticFormString } from './semantic-ui-kinetic-form/string';
+import { SemanticUIKineticFormTextarea } from './semantic-ui-kinetic-form/textarea';
+
+// Although this component allows multiple fields of the same type PageObject
+// has difficulty disambiguating these based on using the components' page
+// object definitions. Either disambiguate in tests by never using same types
+// in a single test or create a new collection manually.
+export const KinenticForm = {
+  scope: '.semantic-ui-kinetic-form',
+  form: SemanticUIKineticFormForm,
+  errorsSection: SemanticUIKineticFormErrors,
+  booleanField: SemanticUIKineticFormBoolean,
+  numberField: SemanticUIKineticFormNumber,
+  radiosField: SemanticUIKineticFormRadios,
+  stringField: SemanticUIKineticFormString,
+  textareaField: SemanticUIKineticFormTextarea,
+  submit() {
+    return this.form.submitButton.click();
+  }
+};
+
+export default PageObject.create(KinenticForm);
