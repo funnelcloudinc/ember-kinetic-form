a55524b75a87649a5be9503266824a693a26ec86
Devin Weaver <suki@tritarget.org>
2017-09-30 19:05:21 -0400

    Add main kinetic-form component

---
+++ addon/components/kinetic-form.js
@@ -0,0 +1,108 @@
+import Ember from 'ember';
+import layout from '../templates/components/kinetic-form';
+import Changeset from 'ember-changeset';
+import lookupValidator from 'ember-changeset-validations';
+import validatorsFor from '../-validators-for';
+
+const {
+  Component, get, set, computed, assign, A
+} = Ember;
+
+const DEFAULT_COMPONENT_NAME_PROP = 'stringComponent';
+
+export default Component.extend({
+  layout,
+  classNames: ['kinetic-form'],
+
+  showErrors: false,
+
+  errorComponent: 'kinetic-form/errors',
+  formComponent: 'kinetic-form/form',
+  stringComponent: 'kinetic-form/string',
+  numberComponent: 'kinetic-form/number',
+  booleanComponent: 'kinetic-form/boolean',
+  radiosComponent: 'kinetic-form/radios',
+  textareaComponent: 'kinetic-form/textarea',
+
+  validators: computed('properties.@each.required', {
+    get() {
+      let validators = {};
+      let properties = get(this, 'properties');
+      for (let property of properties) {
+        validators[property.key] = validatorsFor(property);
+      }
+      return validators;
+    }
+  }),
+
+  changeset: computed('{model,validators}', {
+    get() {
+      let model = get(this, 'model');
+      let validations = get(this, 'validators');
+      let changeset = new Changeset(model, lookupValidator(validations), validations);
+      this.initChangesetInvalidState(changeset);
+      return changeset;
+    }
+  }),
+
+  properties: computed('definition.{schema,form}', {
+    get() {
+      let schema = get(this, 'definition.schema') || {};
+      let form = A(get(this, 'definition.form') || []);
+      let required = A(get(schema, 'required') || []);
+      let properties = get(schema, 'properties') || {};
+
+      return Object.keys(properties).map(key => {
+        let property = assign({}, properties[key]);
+
+        // form property allows for overriding rendering defaults for a given
+        // field type
+        let formType = form.findBy('key', key);
+        let propertyType = formType ?
+          get(formType, 'type') :
+          get(schema, `properties.${key}.type`);
+        let componentName = get(this, `${propertyType}Component`);
+        if (!componentName) {
+          componentName = get(this, DEFAULT_COMPONENT_NAME_PROP);
+        }
+
+        property.key = key;
+        property.required = required.includes(key);
+        property.type = propertyType;
+        property.componentName = componentName;
+        return property;
+      });
+    }
+  }),
+
+  initChangesetInvalidState(changeset) {
+    return changeset.validate()
+      .catch(() => { /* Ignore rejection */ });
+  },
+
+  validateForm() {
+    let changeset = get(this, 'changeset');
+    return changeset.validate()
+      .then(() => set(this, 'showErrors', false))
+      .then(() => changeset)
+      .catch(() => set(this, 'showErrors', true));
+  },
+
+  actions: {
+    updateProperty(key, value) {
+      set(this, `changeset.${key}`, value);
+    },
+
+    submit() {
+      let changeset = get(this, 'changeset');
+      this.validateForm(changeset).then(() => {
+        if (get(changeset, 'isInvalid')) {
+          set(this, 'showErrors', true);
+          return;
+        }
+        set(this, 'showErrors', false);
+        get(this, 'onSubmit')(changeset);
+      });
+    }
+  }
+});
---
+++ tests/pages/components/kinetic-form.js
@@ -0,0 +1,29 @@
+import PageObject from 'ember-cli-page-object';
+import { KineticFormErrors } from './kinetic-form/errors';
+import { KineticFormForm } from './kinetic-form/form';
+import { KineticFormBoolean } from './kinetic-form/boolean';
+import { KineticFormNumber } from './kinetic-form/number';
+import { KineticFormRadios } from './kinetic-form/radios';
+import { KineticFormString } from './kinetic-form/string';
+import { KineticFormTextarea } from './kinetic-form/textarea';
+
+// Although this component allows multiple fields of the same type PageObject
+// has difficulty disambiguating these based on using the components' page
+// object definitions. Either disambiguate in tests by never using same types
+// in a single test or create a new collection manually.
+export const KinenticForm = {
+  scope: '.kinetic-form',
+  form: KineticFormForm,
+  errorsSection: KineticFormErrors,
+  booleanField: KineticFormBoolean,
+  numberField: KineticFormNumber,
+  radiosField: KineticFormRadios,
+  stringField: KineticFormString,
+  textareaField: KineticFormTextarea,
+  submit() {
+    return this.form.submitButton.click();
+  }
+};
+
+export default PageObject.create(KinenticForm);
+
