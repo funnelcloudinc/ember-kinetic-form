61678616b7d18e4a11c430c52b4bbb03ef8b3d2a
Brent Jubinville <brent@marbletank.com>
2017-12-02 23:39:32 -0500

    chore(semantic ui): update classes on form and button

--- addon/components/semantic-ui-kinetic-form/form.js
+++ addon/components/semantic-ui-kinetic-form/form.js
@@ -3,5 +3,5 @@ import layout from '../../templates/components/semantic-ui-kinetic-form/form';
 
 export default KineticFormFormComponent.extend({
   layout,
-  classNames: ['semantic-ui-kinetic-form--form', 'ui', 'form']
+  classNames: ['semantic-ui-kinetic-form--form', 'ui', 'huge', 'form']
 });
