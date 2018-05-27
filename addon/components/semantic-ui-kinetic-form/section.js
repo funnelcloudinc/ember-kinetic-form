623feea52422e6d297d2c93a8d9c7ee7dba9cd16
Devin Weaver <suki@tritarget.org>
2018-05-27 15:18:12 -0400

    feat(sections): Add semantic-ui-kinetic-form/section component

---
+++ addon/components/semantic-ui-kinetic-form/section.js
@@ -0,0 +1,7 @@
+import KineticFormSectionComponent from '../kinetic-form/section';
+import layout from '../../templates/components/semantic-ui-kinetic-form/section';
+
+export default KineticFormSectionComponent.extend({
+  layout,
+  classNames: ['semantic-ui-kinetic-form--section']
+});
