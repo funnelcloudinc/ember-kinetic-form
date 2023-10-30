7083aadaaa422b910028c97c0ad09e4a48898bbc
mdeniverville <marc@funnelcloud.io>
2023-10-30 19:10:57 -0400

    chore(template-linting-errors): turns off onerous octane rules

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -8,5 +8,10 @@ module.exports = {
     'no-invalid-interactive': false,
     'quotes': false,
     'simple-unless': false,
+    // rules introduced with octane
+    'no-action': false,
+    'no-implicit-this': false,
+    'no-curly-component-invocation': false,
+    'require-button-type': false
   }
 };
