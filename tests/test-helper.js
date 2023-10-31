2ab5632a1f153e8c4fe6250c8eb6e007a2789d40
mdeniverville <marc@funnelcloud.io>
2023-10-31 10:15:49 -0400

    chore(ember): upgrade to 3.20

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,6 @@
 import 'ember-sinon-qunit/test-support/test';
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'dummy/app';
+import config from 'dummy/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
