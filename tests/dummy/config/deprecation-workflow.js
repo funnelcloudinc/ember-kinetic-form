562ff5072efd1c3a77ed1b332314757b3befdc38
mdeniverville <marc@funnelcloud.io>
2023-11-01 14:42:43 -0400

    chore(deprecation-workflow): silence deprecations

--- tests/dummy/config/deprecation-workflow.js
+++ tests/dummy/config/deprecation-workflow.js
@@ -11,7 +11,11 @@ self.deprecationWorkflow.config = {
     { handler: "silence", matchId: "ember-cli-page-object.old-collection-api" },
     { handler: "silence", matchId: "ember-views.curly-components.jquery-element" },
     { handler: "silence", matchId: "ember-test-helpers.rendering-context.jquery-element" },
-    { handler: "silence", matchId: "computed-property.override" }
+    { handler: "silence", matchId: "computed-property.override" },
+    // new with upgrade to v3.24
+    { handler: "silence", matchId: "ember-metal.get-with-default" },
+    // new with upgrade to v3.28
+    { handler: "silence", matchId: "this-property-fallback" },
   ]
 };
 /* eslint-enable no-undef */
