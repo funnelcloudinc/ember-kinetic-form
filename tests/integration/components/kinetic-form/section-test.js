b375cba1e2e1a92ea40ce939d77ba5ea43750e6d
mdeniverville <marc@funnelcloud.io>
2024-01-29 10:29:59 -0500

    refactor(kinetic-form-section): recurses to support nested sections

--- tests/integration/components/kinetic-form/section-test.js
+++ tests/integration/components/kinetic-form/section-test.js
@@ -3,7 +3,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import page from '../../../pages/components/kinetic-form/section';
+import page, { pageWithSubSection } from '../../../pages/components/kinetic-form/section';
 
 module('Integration | Component | kinetic form/section', function (hooks) {
   setupRenderingTest(hooks);
@@ -23,15 +23,46 @@ module('Integration | Component | kinetic form/section', function (hooks) {
     assert.strictEqual(page.anchor, 'test-title');
   });
 
-  test('yields field.items', async function (assert) {
-    set(this, 'testField', { title: 'test-title', items: ['1', '2', '3'] });
-    await render(hbs`
-      {{#kinetic-form/section field=this.testField as |item|}}
-        test-{{item}}
-      {{/kinetic-form/section}}
-    `);
-    assert.ok(page.contains('test-1'), 'expected item 1 to be yielded');
-    assert.ok(page.contains('test-2'), 'expected item 2 to be yielded');
-    assert.ok(page.contains('test-3'), 'expected item 3 to be yielded');
+  test('displays section items', async function (assert) {
+    set(this, 'testField', {
+      title: 'test-title',
+      items: [
+        { key: '1', type: 'instructions', title: '1. Heat\n2. Pour', componentName: 'semantic-ui-kinetic-form/instructions' },
+        { key: '2', type: 'passfail', title: 'To be or not', required: false, componentName: 'semantic-ui-kinetic-form/passfail' },
+        { key: '3', type: 'boolean', title: 'Reality [] box', required: true, componentName: 'semantic-ui-kinetic-form/boolean' },
+      ]
+    });
+    set(this, 'noop', function() {})
+    await render(hbs`{{kinetic-form/section field=this.testField update=this.noop}}`);
+
+    assert.ok(page.contains('1. Heat\n2. Pour'), 'expected form element 1 to be rendered');
+    assert.ok(page.contains('To be or not'), 'expected form element 2 to be rendered');
+    assert.ok(page.contains('Reality [] box'), 'expected form element 3 to be rendered');
+  });
+
+  test('displays sub-sections', async function (assert) {
+    set(this, 'testField', {
+      title: 'test-title',
+      items: [
+        { key: '1', type: 'instructions', title: '1. Heat\n2. Pour', componentName: 'semantic-ui-kinetic-form/instructions' },
+        {
+          type: 'section',
+          title: 'sub-section',
+          required: false,
+          componentName: 'kinetic-form/section',
+          items: [
+            { key: '2', type: 'passfail', title: 'To be or not', required: false, componentName: 'semantic-ui-kinetic-form/passfail' },
+            { key: '3', type: 'boolean', title: 'Reality [] box', required: true, componentName: 'semantic-ui-kinetic-form/boolean' },
+          ]
+        }
+      ]
+    });
+    set(this, 'noop', function() {})
+    await render(hbs`<div id="test-parent">{{kinetic-form/section field=this.testField update=this.noop}}</div>`);
+
+    assert.ok(pageWithSubSection.contains('1. Heat\n2. Pour'), 'expected form element 1 to be rendered');
+    assert.ok(pageWithSubSection.contains('sub-section'), 'expected sub-section title to be rendered');
+    assert.ok(pageWithSubSection.contains('To be or not'), 'expected form element 2 to be rendered');
+    assert.ok(pageWithSubSection.contains('Reality [] box'), 'expected form element 3 to be rendered');
   });
 });
