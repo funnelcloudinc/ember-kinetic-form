0fe4a6c246f1c9a9f174ab9a6a47dd82adc1259d
mdeniverville <marc@funnelcloud.io>
2024-01-29 10:30:34 -0500

    refactor(semantic-ui-form-section): recurses to support nested sections

--- tests/pages/components/semantic-ui-kinetic-form/section.js
+++ tests/pages/components/semantic-ui-kinetic-form/section.js
@@ -9,4 +9,13 @@ export const KineticFormSection = {
   hasInTitle: contains('.header'),
 };
 
+const KineticFormWithSubSection = {
+  scope: '#test-parent > .semantic-ui-kinetic-form--section',
+  anchor: attribute('id', 'h2'),
+  title: text('h2'),
+  hasInTitle: contains('h2'),
+};
+const pageWithSubSection = PageObject.create(KineticFormWithSubSection);
+
 export default PageObject.create(KineticFormSection);
+export { pageWithSubSection };
