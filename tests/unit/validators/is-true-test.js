b77cf392ecd82f807ed4ecb0d19ab8d4bcb35337
mdeniverville <95099065+mdeniverville@users.noreply.github.com>
2024-02-28 16:39:02 -0500

    [ENG-5608] Required checkboxes that are not checked are still valid in Operator terminal (#70)
    * refactor(is-true): sends message on validation failure
    
    * chore(version): updates to 2.1.1
    
    * chore(js-lint): fixes linting errors

--- tests/unit/validators/is-true-test.js
+++ tests/unit/validators/is-true-test.js
@@ -4,12 +4,26 @@ import validateIsTrue from 'ember-kinetic-form/validators/is-true';
 
 module('Unit | Validators | is-true', function () {
   test('validates value is true or not', function (assert) {
-    let subject = validateIsTrue();
+    const subject = validateIsTrue();
+    const errorMessagePattern = /must be selected/;
+
     assert.strictEqual(typeOf(subject), 'function');
-    assert.notOk(subject('foo', 'bork'), 'expected return value to be false');
-    assert.notOk(subject('foo', {}), 'expected return value to be false');
-    assert.notOk(subject('foo', 1), 'expected return value to be false');
-    assert.notOk(subject('foo', false), 'expected return value to be false');
+    assert.ok(
+      errorMessagePattern.test(subject('foo', 'bork')),
+      'expected return value to be false'
+    );
+    assert.ok(
+      errorMessagePattern.test(subject('foo', {})),
+      'expected return value to be false'
+    );
+    assert.ok(
+      errorMessagePattern.test(subject('foo', 1)),
+      'expected return value to be false'
+    );
+    assert.ok(
+      errorMessagePattern.test(subject('foo', false)),
+      'expected return value to be false'
+    );
     assert.ok(subject('foo', true), 'expected return value to be true');
   });
 });
